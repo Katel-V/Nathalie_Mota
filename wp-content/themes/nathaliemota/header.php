9a778e5dd035b639d2969d05a8cf03833fb466ee
= <katel.moiriat@gmail.com>
2024-10-04 17:05:04 +0200

    Ajout des templates

---
+++ wp-content/themes/nathaliemota/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="photographe événementiel, photographe event, nathalie mota, photo format hd" />
+	    <meta name="description" content="Nathalie Mota - Site personnel pour la vente de mes photos en impression HD." />
+        <title>Nathalie MOTA</title>
+    </head>
+
+<body>
---
+++ wp-content/themes/nathaliemota/home.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * blog
+ */
+
+ get_header();
+ ?>
+<h1>Nathalie Mota</h1>
+ <?php
+ get_footer();
+ ?>
